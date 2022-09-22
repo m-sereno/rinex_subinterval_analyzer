61f15af8ce0077243304ff057c06c7c6813161da
m-sereno <mateus.sereno@gmail.com>
2022-09-22 16:39:23 -0300

    dictList usage

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from math import sqrt
 from rmse_helper import RmseHelper
 import os, shutil
+
+import csv
+
 inputdir = 'C:\\rinex_subinterval_analyzer\\input'
 outputdir = 'C:\\rinex_subinterval_analyzer\\output'
-
-tableLines = []
-point_baseline_dict = {}
-linesAsVecs = []
-
-pointNamesList = []
-intervalsList = []
-
-isPointTopcon = {}
-
-point_interval_lines = {}
-point_int_count = {}
 
 ignoreCount = {
     "05": 3,
@@ -22,6 +13,12 @@ ignoreCount = {
     "base": 0,
     "default": 1
 }
+
+point_int_count = {}
+pointNamesList = []
+intervalsList = []
+
+slicesData = []
 
 # Preliminar: Limpa tudo
 for filename in os.listdir(outputdir):
@@ -77,98 +74,77 @@ for subdir, dirs, files in os.walk(inputdir):
 
             antennaLine = lines[6]
             isTopcon = antennaLine == "ANTENA NÃO DISPONÍVEL\n"
-            isPointTopcon[pointName] = isTopcon
             
             rinex = lines[1].split(' ')[-1].replace("\n", "")
             utmn = lines[19].split(' ')[-2]
             utme = lines[20].split(' ')[-2]
             hnor = lines[25].split(' ')[-2]
 
-            # === Registrar para calculos posteriores:
-            eqVec = [pointName, interval, rinex, utmn, utme, hnor]
+            # ==== OBJETO DO PONTO:
+            sliceObj = {
+                "name_str": pointName,
+                "int_str": interval,
+                "rinex_str": rinex,
+                "utmn_str": utmn,
+                "utme_str": utme,
+                "hnor_str": hnor,
+                "isTopcon_bool": isTopcon,
+                "dx_num": 0,
+                "dy_num": 0,
+                "dr_num": 0,
+                "dh_num": 0
+            }
 
-            if(interval == "base"):
-                point_baseline_dict[pointName] = eqVec
-            else:
-                linesAsVecs.append(eqVec)
-            
-            # PREPARAR para escrever nos .csv:
-            lineToWrite = ';'.join(eqVec)
-            tableLines.append(lineToWrite)
-
-
-# ESCREVER NO CSV 'BRUTO':
-
-outtablepath = os.path.join(outputdir, 'table.csv')
-
-with open(outtablepath, 'w') as fw:
-    fw.write('\n'.join(tableLines) + '\n')
+            slicesData.append(sliceObj)
 
 
-# ======= CALCULOS POSTERIORES =======
+# ======= CALCULOS =======
 x_rmseHelper = RmseHelper()
 y_rmseHelper = RmseHelper()
 r_rmseHelper = RmseHelper()
 h_rmseHelper = RmseHelper()
 
-for i in range(len(linesAsVecs)):
-    line = linesAsVecs[i]
-    pointName = line[0]
-    interval = line[1]
-    rinex = line[2]
-    utmn = line[3]
-    utme = line[4]
-    hnor = line[5]
+for i in range(len(slicesData)):
+    slice = slicesData[i]
+    pointName = slice["name_str"]
+    interval = slice["int_str"]
+    rinex = slice["rinex_str"]
+    utmn = slice["utmn_str"]
+    utme = slice["utme_str"]
+    hnor = slice["hnor_str"]
 
-    baseline = point_baseline_dict[pointName]
+    if interval == "base":
+        continue
+
+    baseline = next(x for x in slicesData if x["name_str"] == pointName and x["int_str"] == "base")
 
     # Erros:
-    dy = float(utmn.replace(',','.')) - float(baseline[3].replace(',','.'))
-    dx = float(utme.replace(',','.')) - float(baseline[4].replace(',','.'))
+    dy = float(utmn.replace(',','.')) - float(baseline["utmn_str"].replace(',','.'))
+    dx = float(utme.replace(',','.')) - float(baseline["utme_str"].replace(',','.'))
     dr = sqrt(dx*dx + dy*dy)
-    dh = float(hnor.replace(',','.')) - float(baseline[5].replace(',','.'))
+    dh = float(hnor.replace(',','.')) - float(baseline["hnor_str"].replace(',','.'))
 
-    linesAsVecs[i].extend([str(dy).replace('.',','),str(dx).replace('.',','),str(dr).replace('.',','),str(dh).replace('.',',')])
-
-    dict_key = pointName + "###" + interval
-    if dict_key not in point_interval_lines:
-        point_interval_lines[dict_key] = []
-    
-    point_interval_lines[dict_key].append(linesAsVecs[i])
+    slicesData[i]["dy_num"] = dy
+    slicesData[i]["dx_num"] = dx
+    slicesData[i]["dr_num"] = dr
+    slicesData[i]["dh_num"] = dh
 
     x_rmseHelper.registerLine(pointName, int(interval), dx, rinex)
     y_rmseHelper.registerLine(pointName, int(interval), dy, rinex)
     r_rmseHelper.registerLine(pointName, int(interval), dr, rinex)
     h_rmseHelper.registerLine(pointName, int(interval), dh, rinex)
 
+# ESCREVER NO CSV 'BRUTO':
 
-# Create all remaining directories and files:
+outtablepath = os.path.join(outputdir, 'table.csv')
+keys = slicesData[0].keys()
 
-for pointNameStr in pointNamesList:
-    pointpath = os.path.join(outputdir, pointNameStr)
-    os.mkdir(pointpath)
-    for intervalStr in intervalsList:
-        intervalTablePath = os.path.join(pointpath, (intervalStr + ".csv"))
-        with open(intervalTablePath, 'w') as fw:
-            dict_key = pointNameStr + "###" + intervalStr
+with open(outtablepath, 'w', newline='') as fw:
+    dict_writer = csv.DictWriter(fw, keys, delimiter=';')
+    dict_writer.writeheader()
+    dict_writer.writerows(slicesData)
 
-            lines = point_interval_lines[dict_key]
-            fw.write("POINT;INT;RINEX;UTMN;UTME;HNOR;DY;DX;DR;DH\n")
-            for lineVec in lines:
-                fw.write(';'.join(lineVec) + '\n')
-    
-    resultsTablePath = os.path.join(pointpath, "results.csv")
-    with open(resultsTablePath, 'w') as fw:
-        fw.write("INT;RMSE Y;RMSE X;RMSE R;RMSE H\n")
-        for intervalStr in intervalsList:
-            intervalInt = int(intervalStr)
-
-            rmse_x = str(x_rmseHelper.getRmse(pointNameStr, intervalInt)).replace('.',',')
-            rmse_y = str(y_rmseHelper.getRmse(pointNameStr, intervalInt)).replace('.',',')
-            rmse_r = str(r_rmseHelper.getRmse(pointNameStr, intervalInt)).replace('.',',')
-            rmse_h = str(h_rmseHelper.getRmse(pointNameStr, intervalInt)).replace('.',',')
-
-            fw.write(';'.join([intervalStr, rmse_y, rmse_x, rmse_r, rmse_h]) + '\n')
+# Create Summary File:
 
 summaryTablePath = os.path.join(outputdir, "summary.csv")
 with open(summaryTablePath, 'w') as fw:
@@ -193,7 +169,9 @@ with open(summaryTablePath, 'w') as fw:
             (maxE_h, rin_h) = h_rmseHelper.getMaxError(pointNameStr, intervalInt)
             maxE_h = str(maxE_h).replace('.',',')
 
-            fw.write(';'.join([pointNameStr, str(isPointTopcon[pointNameStr]), intervalStr, count,
+            isTopcon = next(x for x in slicesData if x["int_str"] == intervalStr)["isTopcon_bool"]
+
+            fw.write(';'.join([pointNameStr, str(isTopcon), intervalStr, count,
                 rmse_y, rmse_x, rmse_r, rmse_h,
                 maxE_y, maxE_x, maxE_r, maxE_h,
                 rin_y, rin_x, rin_r, rin_h]) + '\n')

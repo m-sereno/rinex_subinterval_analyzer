d9a3b9b0c216b07d4dca19748d7f35030265f4ec
m-sereno <mateus.sereno@gmail.com>
2022-10-10 16:27:41 -0300

    analysis for everything together

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 from analyzer import run_analysis
 
-# Run the analysis 2 times:
-    # Once for topcon, once for trimble
-
-run_analysis({"isTopcon_bool": True}, "TOPCON")
-
-run_analysis({"isTopcon_bool": False}, "TRIMBLE")
-
-
-# Run the analysis for everything together:
-
 run_analysis({}, "ALL")
